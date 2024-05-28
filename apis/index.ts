11fa2154907439641cf6be4d866e5b9fead06815
Marcel Klehr <mklehr@gmx.net>
2024-05-28 10:48:30 +0200

    Initial commit

---
+++ apis/index.ts
@@ -0,0 +1,26 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AppPasswordApi';
+export * from './AutoCompleteApi';
+export * from './AvatarApi';
+export * from './ClientFlowLoginV2Api';
+export * from './CollaborationResourcesApi';
+export * from './DefaultApi';
+export * from './GuestAvatarApi';
+export * from './HoverCardApi';
+export * from './LoginApi';
+export * from './NavigationApi';
+export * from './OcmApi';
+export * from './OcsApi';
+export * from './PreviewApi';
+export * from './ProfileApiApi';
+export * from './ReferenceApi';
+export * from './ReferenceApiApi';
+export * from './TaskProcessingApiApi';
+export * from './TeamsApiApi';
+export * from './TextProcessingApiApi';
+export * from './TextToImageApiApi';
+export * from './TranslationApiApi';
+export * from './UnifiedSearchApi';
+export * from './WhatsNewApi';
+export * from './WipeApi';
