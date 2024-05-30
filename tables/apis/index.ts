c6eb69ec330a35c26b319024a80c48dc40557afb
Marcel Klehr <mklehr@gmx.net>
2024-05-30 10:02:46 +0200

    Add tables APIs

---
+++ tables/apis/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Api1Api.ts';
+export * from './ApiColumnsApi.ts';
+export * from './ApiFavoriteApi.ts';
+export * from './ApiGeneralApi.ts';
+export * from './ApiTablesApi.ts';
+export * from './ContextApi.ts';
