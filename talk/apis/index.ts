a5732aa7e242c7505d6d15a5e52adabdc320039b
Marcel Klehr <mklehr@gmx.net>
2024-06-19 10:52:26 +0200

    feat: Talk API

---
+++ talk/apis/index.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AvatarApi.ts';
+export * from './BanApi.ts';
+export * from './BotApi.ts';
+export * from './BreakoutRoomApi.ts';
+export * from './CallApi.ts';
+export * from './ChatApi.ts';
+export * from './ExternalSignalingApi.ts';
+export * from './FilesIntegrationApi.ts';
+export * from './GuestApi.ts';
+export * from './InternalSignalingApi.ts';
+export * from './MatterbridgeApi.ts';
+export * from './PollApi.ts';
+export * from './ReactionApi.ts';
+export * from './RecordingApi.ts';
+export * from './RoomApi.ts';
+export * from './SettingsApi.ts';
+export * from './UserAvatarApi.ts';
