909118a9e851e9a042029ae915122840150d7315
Marcel Klehr <mklehr@gmx.net>
2024-06-19 13:19:06 +0200

    Add notifications API client

---
+++ notifications/models/index.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Capabilities.ts';
+export * from './CapabilitiesNotifications.ts';
+export * from './EndpointConfirmIdsForUser200Response.ts';
+export * from './EndpointConfirmIdsForUser200ResponseOcs.ts';
+export * from './EndpointDeleteAllNotifications200Response.ts';
+export * from './EndpointDeleteAllNotifications200ResponseOcs.ts';
+export * from './EndpointDeleteAllNotifications403Response.ts';
+export * from './EndpointDeleteAllNotifications403ResponseOcs.ts';
+export * from './EndpointGetNotification200Response.ts';
+export * from './EndpointGetNotification200ResponseOcs.ts';
+export * from './EndpointListNotifications200Response.ts';
+export * from './EndpointListNotifications200ResponseOcs.ts';
+export * from './Notification.ts';
+export * from './NotificationAction.ts';
+export * from './OCSMeta.ts';
