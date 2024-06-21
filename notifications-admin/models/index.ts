4432705a6ee46808e951b08cf3e9e3e1daece0f9
Marcel Klehr <mklehr@gmx.net>
2024-06-21 10:20:09 +0200

    Add notifications admin

---
+++ notifications-admin/models/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ApiGenerateNotification200Response.ts';
+export * from './ApiGenerateNotification200ResponseOcs.ts';
+export * from './ApiGenerateNotification400Response.ts';
+export * from './ApiGenerateNotification400ResponseOcs.ts';
+export * from './Capabilities.ts';
+export * from './CapabilitiesNotifications.ts';
+export * from './OCSMeta.ts';
